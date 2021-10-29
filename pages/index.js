920f156004aa2b61dcc004455fab69cd742fcfb1
mike wen <mike@MIKEs-MacBook-Air.local>
2021-10-29 03:26:56 -0400

    tweak copy

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex h-screen">
       <div className="my-auto ml-4 md:ml-32 lg:ml-80">
         <h1 className="font-serif italic text-6xl">Mike Wen</h1>
-        <p className="mt-2 text-sm w-96 leading-6">Making a dent. Pursuing the greater good. Trying my best. Founder of <a className="hover:text-gray-400 hover:underline" href="https://gallery.so/mikey">Gallery.</a> <a className="hover:text-gray-400 hover:underline" href="https://twitter.com/mikeybitcoin">Twitter.</a> <a className="hover:text-gray-400 hover:underline" href="https://instagram.com/mikeybitcoin">Insta.</a></p>
+        <p className="mt-2 text-sm w-96 leading-6">Making a dent. Pursuing a greater good. Trying my best. Founder of <a className="hover:text-gray-400 hover:underline" href="https://gallery.so/mikey">Gallery.</a> <a className="hover:text-gray-400 hover:underline" href="https://twitter.com/mikeybitcoin">Twitter.</a> <a className="hover:text-gray-400 hover:underline" href="https://instagram.com/mikeybitcoin">Insta.</a></p>
         {/* <p className="mt-2 text-sm"><a className="hover:text-gray-400 hover:underline" href="https://twitter.com/mikeybitcoin">Twitter.</a> <a className="hover:text-gray-400 hover:underline" href="https://instagram.com/mikeybitcoin">Insta.</a></p> */}
       </div>
     </div>
